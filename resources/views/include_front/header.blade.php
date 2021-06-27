5ae3ef4298a5bc2be727c1057898a34442745107
syahalhafid <syahalhafidnugroho@users.noreply.github.com>
2021-06-28 02:12:40 +0700

    nama icon

--- resources/views/include_front/header.blade.php
+++ resources/views/include_front/header.blade.php
@@ -38,8 +38,7 @@
 	<body data-spy="scroll" style="height: 100vh;" data-target="#site-navbar" data-offset="200">
 		<nav class="navbar navbar-expand-lg navbar-dark site_navbar bg-light site-navbar-light" id="site-navbar">
 			<div class="container">
-				<a class="navbar-brand" href="/front"><img src="{{asset('Gambar/logo.png')}}" alt="AdminLTE Logo" class="brand-image" height="1%" width="25%" alt="" srcset="" style="opacity: 1">
-				</a>
+				<a class="navbar-brand" href="/front">CAFEGUE</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#site-nav" aria-controls="site-nav" aria-expanded="false" aria-label="Toggle navigation">
 				  <span class="oi oi-menu"></span> Menu
 				</button>
