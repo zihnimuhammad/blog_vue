df732b5be4710fa9a42fd270f66dcb96247180a7
syahalhafid <syahalhafidnugroho@users.noreply.github.com>
2021-07-31 13:35:07 +0700

    beranda

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class BerandaController extends Controller
 {
     public function index()
     {
-        return view('Admin.HalamanDepan.Beranda');
+        $karyawan       =   DB::table('karyawan')->count();
+        $menu_hidangan  =   DB::table('menu_hidangan')->count();
+        $pesanan       =   DB::table('pesanan')->count();
+        $jenis_penyakit       =   DB::table('jenis_penyakit')->count();
+        return view('Admin.HalamanDepan.Beranda',compact('karyawan','menu_hidangan','pesanan','jenis_penyakit'));
+    }
+    public function barChart(){
+        $users  = User::select(DB::raw("COUNT(*) as count"))
+                ->whereYear('created_at', date('Y'))
+                ->groupBy(DB::raw("Month(created_at)"))
+                ->pluck('count');
+        $months = User::select(DB::raw("COUNT(*) as count"))
+                ->whereYear('created_at', date('Y'))
+                ->groupBy(DB::raw("Month(created_at)"))
+                ->pluck('count');
+
+        $datas  = array(0,0,0,0,0,0,0,0,0,0,0,0);
+        foreach($months as $index => $months)
+        {
+            $datas[$months] = $users[$index];
+        }
+
+        return view('bar-chart',compact('datas'));
     }
 }
--- resources/views/Admin/Template/script.blade.php
+++ resources/views/Admin/Template/script.blade.php
@@ -4,3 +4,16 @@
 <script src="{{asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('AdminLTE/dist/js/adminlte.min.js')}}"></script>
+
+
+<script src="{{asset('AdminLTE/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE -->
+<script src="{{asset('AdminLTE/dist/js/adminlte.js')}}"></script>
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{asset('AdminLTE/plugins/chart.js/Chart.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('AdminLTE/dist/js/demo.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('AdminLTE/dist/js/pages/dashboard3.js')}}"></script>
